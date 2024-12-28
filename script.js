c12801e8a9cc99c4b1fc50023de047bd018d3cba
Jambulat Myagchiev <179936292+Myagchiev@users.noreply.github.com>
2024-12-28 22:27:05 +0300

    second

---
+++ script.js
@@ -0,0 +1,46 @@
+  function play(playerSelection) {
+    const choices = ['Камень', 'Ножницы', 'Бумага'];
+    const getComputerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let result;
+
+    if (playerSelection === getComputerChoice) {
+        result = "Ничья! Оба выбрали " + playerSelection + ".";
+    } else if (
+        (playerSelection === 'Камень' && getComputerChoice === 'Ножницы') ||
+        (playerSelection === 'Ножницы' && getComputerChoice === 'Бумага') ||
+        (playerSelection === 'Бумага' && getComputerChoice === 'Камень')
+    ) {
+        result = "Вы выиграли! " + playerSelection + " побеждает " + getComputerChoice + ".";
+    } else {
+        result = "Вы проиграли! " + getComputerChoice + " побеждает " + playerSelection + ".";
+    }
+
+    document.getElementById('result').innerText = result;
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function play(playerSelection) {
+    const choices = ['Камень', 'Ножницы', 'Бумага'];
+    const getComputerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let result;
+
+    if (playerSelection === getComputerChoice) {
+        result = "Ничья! Оба выбрали " + playerSelection + ".";
+    } else if (
+        (playerSelection === 'Камень' && getComputerChoice === 'Ножницы') ||
+        (playerSelection === 'Ножницы' && getComputerChoice === 'Бумага') ||
+        (playerSelection === 'Бумага' && getComputerChoice === 'Камень')
+    ) {
+        result = "Вы выиграли! " + playerSelection + " побеждает " + getComputerChoice + ".";
+        playerScore++;
+    } else {
+        result = "Вы проиграли! " + getComputerChoice + " побеждает " + playerSelection + ".";
+        computerScore++;
+    }
+
+    document.getElementById('result').innerText = result;
+    document.getElementById('playerScore').innerText = "Счет игрока: " + playerScore;
+    document.getElementById('computerScore').innerText = "Счет компьютера: " + computerScore;
+}
