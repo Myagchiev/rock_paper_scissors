ba5aef61cab014fdaf6f9d048b8e2754d0722054
Jambulat Myagchiev <179936292+Myagchiev@users.noreply.github.com>
2024-12-28 22:34:48 +0300

    third

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-  function play(playerSelection) {
-    const choices = ['Камень', 'Ножницы', 'Бумага'];
-    const getComputerChoice = choices[Math.floor(Math.random() * choices.length)];
-    let result;
-
-    if (playerSelection === getComputerChoice) {
-        result = "Ничья! Оба выбрали " + playerSelection + ".";
-    } else if (
-        (playerSelection === 'Камень' && getComputerChoice === 'Ножницы') ||
-        (playerSelection === 'Ножницы' && getComputerChoice === 'Бумага') ||
-        (playerSelection === 'Бумага' && getComputerChoice === 'Камень')
-    ) {
-        result = "Вы выиграли! " + playerSelection + " побеждает " + getComputerChoice + ".";
-    } else {
-        result = "Вы проиграли! " + getComputerChoice + " побеждает " + playerSelection + ".";
-    }
-
-    document.getElementById('result').innerText = result;
-}
-
 let playerScore = 0;
 let computerScore = 0;
 
