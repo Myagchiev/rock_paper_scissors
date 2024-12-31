c54f6329b615e22bd2f10558346ac4e0592163ea
Jambulat Myagchiev <179936292+Myagchiev@users.noreply.github.com>
2024-12-31 03:28:51 +0300

    new

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 let playerScore = 0;
 let computerScore = 0;
 
-function play(playerSelection) {
-    const choices = ['Камень', 'Ножницы', 'Бумага'];
-    const getComputerChoice = choices[Math.floor(Math.random() * choices.length)];
-    let result;
+const ROCK = 'Камень';
+const SCISSORS = 'Ножницы';
+const PAPER = 'Бумага';
+const choices = [ROCK, SCISSORS, PAPER];
 
-    if (playerSelection === getComputerChoice) {
-        result = "Ничья! Оба выбрали " + playerSelection + ".";
+function play(playerSelection) {
+    if (!choices.includes(playerSelection)) {
+        document.getElementById('result').innerText = "Неверный выбор. Выберите Камень, Ножницы или Бумага.";
+        return;
+    }
+
+    const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    let result = playerSelection + " против " + computerSelection + ". ";
+
+    if (playerSelection === computerSelection) {
+        result += "Ничья!";
     } else if (
-        (playerSelection === 'Камень' && getComputerChoice === 'Ножницы') ||
-        (playerSelection === 'Ножницы' && getComputerChoice === 'Бумага') ||
-        (playerSelection === 'Бумага' && getComputerChoice === 'Камень')
+        (playerSelection === ROCK && computerSelection === SCISSORS) ||
+        (playerSelection === SCISSORS && computerSelection === PAPER) ||
+        (playerSelection === PAPER && computerSelection === ROCK)
     ) {
-        result = "Вы выиграли! " + playerSelection + " побеждает " + getComputerChoice + ".";
+        result += "Вы выиграли!";
         playerScore++;
     } else {
-        result = "Вы проиграли! " + getComputerChoice + " побеждает " + playerSelection + ".";
+        result += "Вы проиграли!";
         computerScore++;
     }
 
     document.getElementById('result').innerText = result;
+    updateScores();
+}
+
+function updateScores() {
     document.getElementById('playerScore').innerText = "Счет игрока: " + playerScore;
     document.getElementById('computerScore').innerText = "Счет компьютера: " + computerScore;
 }
